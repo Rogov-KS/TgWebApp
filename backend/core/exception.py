eb2e375dd8acb471770da4e2284b5b9be0c66b3c
Rogov Konstantin <rogov.ks@phystech.edu>
2025-06-28 14:09:43 +0300

    change exceptions by classes

--- backend/core/exception.py
+++ backend/core/exception.py
@@ -1,40 +1,49 @@
 from fastapi import HTTPException, status
 
-UserAlreadyExistsException = HTTPException(
-    status_code=status.HTTP_400_BAD_REQUEST,
-    detail="User already exists",
-)
 
-UserNotFoundException = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND,
-)
+class TgWebAppException(HTTPException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    detail = ""
 
-InvalidTelegramIdOrPasswordException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Invalid telegram id or password",
-)
+    def __init__(self) -> None:
+        super().__init__(status_code=self.status_code, detail=self.detail)
 
-ForbiddenException = HTTPException(
-    status_code=status.HTTP_403_FORBIDDEN,
-    detail="Forbidden",
-)
 
-NotAuthenticatedException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Not authenticated",
-)
+class UserAlreadyExistsException(TgWebAppException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = "User already exists"
 
-TokenExpiredException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Token expired",
-)
 
-TokenAbsentException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Token absent",
-)
+class UserNotFoundException(TgWebAppException):
+    status_code = status.HTTP_404_NOT_FOUND
+    detail = "User not found"
 
-IncorrectTokenFormatException = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Incorrect token format",
-)
+
+class InvalidTelegramIdOrPasswordException(TgWebAppException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Invalid telegram id or password"
+
+
+class ForbiddenException(TgWebAppException):
+    status_code = status.HTTP_403_FORBIDDEN
+    detail = "Forbidden"
+
+
+class NotAuthenticatedException(TgWebAppException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Not authenticated"
+
+
+class TokenExpiredException(TgWebAppException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Token expired"
+
+
+class TokenAbsentException(TgWebAppException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Token absent"
+
+
+class IncorrectTokenFormatException(TgWebAppException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Incorrect token format"
