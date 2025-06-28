cd8175b92d5936c5fc567f1794cf3399e7a9d232
Rogov Konstantin <rogov.ks@phystech.edu>
2025-06-28 18:32:16 +0300

    add leaderbord api endpoint

--- backend/api/endpoints/leaderboard.py
+++ backend/api/endpoints/leaderboard.py
@@ -1,3 +1,17 @@
 from fastapi import APIRouter
 
+from backend.dao.utils import get_db_leaderboard
+from backend.schemas.leaderboard import LeaderboardPlace
+
 router = APIRouter(prefix="/leaderboard", tags=["Leaderboard"])
+
+
+@router.get("/")
+async def get_leaderboard(
+    limit: int = 10,
+    offset: int = 0,
+    sort_order: str = "desc",
+) -> list[LeaderboardPlace]:
+    """Получить топ игроков в рейтинге."""
+    top_scores = await get_db_leaderboard(limit, offset, sort_order)
+    return top_scores
--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ import sys
 
 from fastapi import FastAPI
 
-from backend.api.endpoints import auth, game_sessions, users
+from backend.api.endpoints import auth, game_sessions, leaderboard, users
 from backend.logger import get_logger, setup_logging
 
 # Создаем экземпляр FastAPI
@@ -12,6 +12,7 @@ app = FastAPI(title="TgWebApp API", version="1.0.0")
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(game_sessions.router)
+app.include_router(leaderboard.router)
 
 logger = get_logger(__name__)
 
