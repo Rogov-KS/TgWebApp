6a3cb19210b8282e0ea1c1a459990f1634506584
Rogov Konstantin <rogov.ks@phystech.edu>
2025-08-21 20:34:14 +0300

    making tests - 8

--- tests/backend/fixtures/database.py
+++ tests/backend/fixtures/database.py
@@ -17,56 +17,59 @@ from backend.core.database import Base
 settings = get_settings(env_files=["envs/.env-base", "envs/.env-test"])
 
 
-@pytest_asyncio.fixture(scope="session")
+async def create_tables(engine: AsyncEngine):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        print("✅ Все таблицы созданы успешно")
+
+
+async def drop_tables(engine: AsyncEngine):
+    try:
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            print("🗑️ Все таблицы удалены успешно")
+    except Exception as e:
+        print(f"Ошибка при удалении таблиц: {e}")
+
+
+
+@pytest_asyncio.fixture(scope="function")
 async def test_db_engine() -> AsyncGenerator[AsyncEngine, None]:
     """
     Фикстура для создания тестового движка базы данных.
     Запускается один раз в начале сессии тестов.
     """
-    import asyncio
-    current_loop = asyncio.get_running_loop()
-    print(f"Current event loop (async def test_db_engine): {current_loop}")
     print(f"{settings.MODE=}")
     assert settings.MODE == "TEST"
     test_db_url = settings.DATABASE_URL
 
     engine = create_async_engine(test_db_url, echo=False)
 
-    # Создаем все таблицы в начале сессии
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-        print("✅ Все таблицы созданы успешно")
+    await create_tables(engine)
 
-    yield engine
+    try:
+        yield engine
+    finally:
+        await drop_tables(engine)
+        await engine.dispose()
 
-    # Удаляем все таблицы в конце сессии
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        print("🗑️ Все таблицы удалены успешно")
 
-    await engine.dispose()
-
-@pytest_asyncio.fixture(scope="session")
+@pytest_asyncio.fixture(scope="function")
 async def test_db_session_maker(test_db_engine: AsyncEngine) -> async_sessionmaker:
     """
     Фикстура для создания тестового движка базы данных.
     Запускается один раз в начале сессии тестов.
     """
-    import asyncio
-    current_loop = asyncio.get_running_loop()
-    print(f"Current event loop (async def test_db_session): {current_loop}")
     session_maker = async_sessionmaker(test_db_engine, expire_on_commit=False)
     return session_maker
 
-@pytest_asyncio.fixture
+
+@pytest_asyncio.fixture(scope="function")
 async def test_db_session(test_db_session_maker: async_sessionmaker) -> AsyncGenerator[AsyncSession, None]:
     """
     Фикстура для создания сессии БД для каждого теста.
     Автоматически создает и закрывает сессию для каждого теста.
     """
-    import asyncio
-    current_loop = asyncio.get_running_loop()
-    print(f"Current event loop (async def test_db_session): {current_loop}")
     async with test_db_session_maker() as session:
         try:
             yield session
